f49432e0a2b20904792687a8fe4128f1633ab243
sagdutt <sagdutt@gmail.com>
2016-11-23 13:28:05 +0530

    updated user profile

--- app/user/user-controller.js
+++ app/user/user-controller.js
@@ -4,7 +4,32 @@ angular.module('Social')
 		$scope.editable = false;
 		if($rootScope.id == $scope.user_id)
 			$scope.editable = true;
-		$scope.editProfile = function(){
-			console.log("editing user"+$scope.id);
-		}
+		
+		$scope.userDetails = {
+			firstName: "abc",
+			lastName: "def",
+			currentOrganization: "NRI fintech",
+			currentDesignation: "CEO",
+			email: 'abc@abc.com',
+		};
+
+		$scope.works = [
+			{
+				organization: "NRI fintech",
+				designation: "CEO",
+				startDate: new Date("7-1-2017"),
+				endDate: new Date("7-1-2019"),
+				description: "I rule this shit."
+			}
+		];
+
+		$scope.educations = [
+			{
+				institute: "Institute of Engineering and Management",
+				degree: "B. Tech in Computer Science",
+				startDate: new Date("7-1-2013"),
+				endDate: new Date("6-30-2017"),
+				description: "Had a shitload of fun."
+			}
+		];
 	});
